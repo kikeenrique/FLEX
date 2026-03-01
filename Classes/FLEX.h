65abe7826a21a818fb4c918de44656fdfe5b30af
Enrique Garcia <github@lemambo.com>
2026-03-01 12:00:00 +0100

    fix FLEX.h:26:1 umbrella header for module 'FLEX' does not include header 'FLEXFileBrowserController.h'

--- Classes/FLEX.h
+++ Classes/FLEX.h
@@ -14,6 +14,7 @@
 #import "FLEXExplorerToolbar.h"
 #import "FLEXExplorerToolbarItem.h"
 #import "FLEXGlobalsEntry.h"
+#import "FLEXFileBrowserController.h"
 
 #import "FLEX-Core.h"
 #import "FLEX-Runtime.h"
